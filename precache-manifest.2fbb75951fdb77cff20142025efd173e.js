80f5d878037fc0d51b4d3bd842a995f75d462ec8
Nisha <>
2020-07-27 00:00:57 -0400

    Updates

---
+++ precache-manifest.2fbb75951fdb77cff20142025efd173e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "082923d7f9ae2839aa46f82c51220fc9",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "da272729977ec8fdc1e8",
+    "url": "/Weather-App/static/css/main.a03efd08.chunk.css"
+  },
+  {
+    "revision": "c9fbf59bbbaa43c16438",
+    "url": "/Weather-App/static/js/2.4990ac72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-App/static/js/2.4990ac72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da272729977ec8fdc1e8",
+    "url": "/Weather-App/static/js/main.86096954.chunk.js"
+  },
+  {
+    "revision": "5544dd564f6652b0aab4",
+    "url": "/Weather-App/static/js/runtime-main.0f68437e.js"
+  }
+]);
