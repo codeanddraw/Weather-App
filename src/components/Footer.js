3218c3162d6dc6e09bde3f274f10399b64727718
Nisha <nisha@Nishas-MacBook-Pro.local>
2020-07-26 23:21:25 -0400

    Add scss styles to the forecast app

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,7 @@ import React from 'react'
 
 const Footer = () =>{
     return <div className="footer">
-        <p>Designed and developed by 
-            <a rel="noopener noreferrer" target="_blank" href="www.linkedin.com/in/NishaChaube">Nisha Chaube</a>
+        <p>Designed and developed by Nisha Chaube
         </p>
     </div>
 }
